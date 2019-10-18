5ebac1132a3e2e06379df759f004e70440776fb2
Laurent Michel <laurent.michel@ruag.com>
2019-10-18 17:05:12 +0200

    A job to clean up all the e2e environments would be appreciated #27
    * add pipeline to clean up e2e envs
    
    https://github.com/shopozor/ci/issues/27

---
+++ CleanupE2eEnvs.groovy
@@ -0,0 +1,19 @@
+pipeline {
+  agent any
+  environment {
+    BACKEND_NAME = credentials('backend-name-credentials') // contains envName + base jps url
+    FRONTEND_NAME = credentials('frontend-name-credentials') // contains envName
+    JELASTIC_APP_CREDENTIALS = credentials('jelastic-app-credentials')
+    JELASTIC_CREDENTIALS = credentials('jelastic-credentials')
+  }
+  stages {
+    stage('Clean up environments') {
+      steps {
+        script = "delete-jelastic-env.sh"
+        sh "chmod u+x $script"
+        sh "$script ${JELASTIC_APP_CREDENTIALS_USR} ${JELASTIC_APP_CREDENTIALS_PSW} ${JELASTIC_CREDENTIALS_USR} ${JELASTIC_CREDENTIALS_PSW} ${BACKEND_NAME_USR}"
+        sh "$script ${JELASTIC_APP_CREDENTIALS_USR} ${JELASTIC_APP_CREDENTIALS_PSW} ${JELASTIC_CREDENTIALS_USR} ${JELASTIC_CREDENTIALS_PSW} ${FRONTEND_NAME_USR}"
+      }
+    }
+  }
+}
